6b085af26eed5efb2be1116182646e1cdb9014f8
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2020-07-15 13:10:38 -0300

    Adicionando as funções para Ouvir a frase e lingua

---
+++ sample/helpers.py
@@ -0,0 +1,58 @@
+# importa a Lib que fará o reconhecimento de voz
+import speech_recognition as sr
+
+# Funcao responsavel por ouvir e reconhecer a fala
+
+
+def ouvirFrase():
+    '''
+      Função para retornar a frase obtida pelo programa atravez da lib speech_recognition
+      :return: Retornara a frase obtida pelo reconhecimento de voz ou -1 se houver algum erro
+    '''
+    # Habilita o microfone para ouvir o usuario
+    ouvinte = sr.Recognizer()
+    with sr.Microphone() as source:
+        # Usa da função presente na speech_recognition para diminuir o ruido do microfone
+        ouvinte.adjust_for_ambient_noise(source)
+        # Avisa ao usuario que que esta pronto para ouvir
+        print("Diga me o que devo traduzir: ")
+        # Armazena o audio na variavel
+        audio = ouvinte.listen(source)
+        try:
+            # Passa o audio para o reconhecedor de padroes do speech_recognition
+            frase = ouvinte.recognize_google(audio, language='pt-BR')
+            # Após alguns segundos, retorna a frase falada
+            print("Você disse: " + frase)
+        # Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
+        except sr.UnkownValueError:
+            print("Não entendi")
+            return -1
+    return frase
+
+
+def ouvirLingua():
+  '''
+    Função para retornar a lingua obtida pelo programa atravez da lib speech_recognition
+    :return: Retornara a lingua obtida pelo reconhecimento de voz ou -1 se houver algum erro
+  '''
+  # Habilita o microfone para ouvir o usuario
+  ouvinte = sr.Recognizer()
+  with sr.Microphone() as source:
+    # Usa da função presente na speech_recognition para diminuir o ruido do microfone
+    ouvinte.adjust_for_ambient_noise(source)
+
+    # Avisa ao usuario que que esta pronto para ouvir
+    print("Diga me o que devo traduzir: ")
+    # Armazena o audio na variavel
+    audio = ouvinte.listen(source)
+    try:
+      # Passa o audio para o reconhecedor de padroes do speech_recognition
+      frase = ouvinte.recognize_google(audio, language='pt-BR')
+      # Após alguns segundos, retorna a frase falada
+      print("Você disse: " + frase)
+      # Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
+    except sr.UnkownValueError:
+      print("Não entendi")
+      return -1
+  if frase == 'português':
+    return 'pt-br'
