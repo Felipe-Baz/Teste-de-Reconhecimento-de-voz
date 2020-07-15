fb5129fe6e36062d882980f5f6048b38cd656e2e
Felipe Baz <54612024+Felipe-Baz@users.noreply.github.com>
2020-07-15 13:17:24 -0300

    corrigindo indentação

--- sample/helpers.py
+++ sample/helpers.py
@@ -5,29 +5,29 @@ import speech_recognition as sr
 
 
 def ouvirFrase():
-    '''
-      Função para retornar a frase obtida pelo programa atravez da lib speech_recognition
-      :return: Retornara a frase obtida pelo reconhecimento de voz ou -1 se houver algum erro
-    '''
-    # Habilita o microfone para ouvir o usuario
-    ouvinte = sr.Recognizer()
-    with sr.Microphone() as source:
-        # Usa da função presente na speech_recognition para diminuir o ruido do microfone
-        ouvinte.adjust_for_ambient_noise(source)
-        # Avisa ao usuario que que esta pronto para ouvir
-        print("Diga me o que devo traduzir: ")
-        # Armazena o audio na variavel
-        audio = ouvinte.listen(source)
-        try:
-            # Passa o audio para o reconhecedor de padroes do speech_recognition
-            frase = ouvinte.recognize_google(audio, language='pt-BR')
-            # Após alguns segundos, retorna a frase falada
-            print("Você disse: " + frase)
-        # Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
-        except sr.UnkownValueError:
-            print("Não entendi")
-            return -1
-    return frase
+  '''
+    Função para retornar a frase obtida pelo programa atravez da lib speech_recognition
+    :return: Retornara a frase obtida pelo reconhecimento de voz ou -1 se houver algum erro
+  '''
+  # Habilita o microfone para ouvir o usuario
+  ouvinte = sr.Recognizer()
+  with sr.Microphone() as source:
+    # Usa da função presente na speech_recognition para diminuir o ruido do microfone
+    ouvinte.adjust_for_ambient_noise(source)
+    # Avisa ao usuario que que esta pronto para ouvir
+    print("Diga me o que devo traduzir: ")
+    # Armazena o audio na variavel
+    audio = ouvinte.listen(source)
+    try:
+      # Passa o audio para o reconhecedor de padroes do speech_recognition
+      frase = ouvinte.recognize_google(audio, language='pt-BR')
+      # Após alguns segundos, retorna a frase falada
+      print("Você disse: " + frase)
+      # Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
+    except sr.UnkownValueError:
+      print("Não entendi")
+      return -1
+  return frase
 
 
 def ouvirLingua():
